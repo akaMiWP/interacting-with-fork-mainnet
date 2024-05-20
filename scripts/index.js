b5b1c16cba6a3ca749909ea9dacfe26c915e9f49
pepukrit <pepukrit@hotmail.com>
2024-05-20 16:46:42 +0700

    Added an interactive file to print out sepolia USDC balance of the deployer account

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const USDC_Address = "0x2c032aa43d119d7bf4adc42583f1f94f3bf3023a";
+  const IERC20 = await ethers.getContractFactory("IERC20");
+  const USDC_contract = await IERC20.attach(USDC_Address);
+  const balance = await USDC_contract.balanceOf(deployer);
+  console.log(`USDC Balance: ${ethers.parseUnits(balance, "ethers")} DAI`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
