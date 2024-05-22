b1520b5d04420221c760f06faca4bf0aa72d8a7a
akaMiWP <bangkokappdevelopment@gmail.com>
2024-05-22 16:12:12 +0700

    Set up a hardhat config to connect with the node

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,4 +4,10 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: { mnemonic: process.env.MNEMONICS },
+    },
+  },
 };
--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,16 @@ const { ethers } = require("hardhat");
 async function main() {
   const [deployer] = await ethers.getSigners();
   const USDC_Address = "0x2c032aa43d119d7bf4adc42583f1f94f3bf3023a";
-  const IERC20 = await ethers.getContractFactory("IERC20");
-  const USDC_contract = await IERC20.attach(USDC_Address);
-  const balance = await USDC_contract.balanceOf(deployer);
-  console.log(`USDC Balance: ${ethers.parseUnits(balance, "ethers")} DAI`);
+  // const IERC20 = await ethers.getContractFactory("IERC20");
+  // const USDC_contract = await IERC20.attach(USDC_Address);
+  // const balance = await USDC_contract.balanceOf(deployer);
+  // console.log(`USDC Balance: ${ethers.parseUnits(balance, "ethers")} DAI`);
+  console.log(`Deployer: ${await deployer.getAddress()}`);
+
+  let ethBalance = await ethers.provider.getBalance(
+    await deployer.getAddress()
+  );
+  console.log(`ETH balance: ${ethers.formatUnits(ethBalance, "ether")}`);
 }
 
 main()
